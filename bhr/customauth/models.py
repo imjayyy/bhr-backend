88337a2b39241e85a1e132f9b98a1c8cabc3d482
Mujtaba <macbook@MacBooks-MacBook-Pro.local>
2024-09-17 03:26:05 +0500

    auth updated

--- bhr/customauth/models.py
+++ bhr/customauth/models.py
@@ -1,6 +1,3 @@
-
-
-
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin, AbstractUser
 from django.db import models
 from django.utils.translation import gettext_lazy as _
